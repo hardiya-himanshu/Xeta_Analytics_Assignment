85a68b3c5ec733def67670badb67d31804ad7949
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2024-09-28 16:32:14 +0530

    header own theme code

--- fitsnap/src/components/header/Header.jsx
+++ fitsnap/src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
-import FitsnapTheme from '../../utils/FitsnapTheme.jsx'
 import { Link as ScrollLink} from 'react-scroll'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../../store/themeSlice.js'
 
-
-function Header({darkMode, onToggleTheme}) {
+function Header() {
     const [showMenu, setShowMenu] = useState(false)
+    const darkMode = useSelector((state) => state.theme.darkMode);
+    const dispatch = useDispatch();
+    const handleTheme = () => {
+        dispatch(toggleTheme());
+    };
     const navItems = [
         {
           name: "Features",
@@ -56,11 +61,11 @@ function Header({darkMode, onToggleTheme}) {
 
             <div className='flex items-center'>
               {darkMode?
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 cursor-pointer" onClick={onToggleTheme}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 cursor-pointer" onClick={handleTheme}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                 </svg>
                 :
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 cursor-pointer" onClick={onToggleTheme}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 cursor-pointer" onClick={handleTheme}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                 </svg>
               }
@@ -110,4 +115,4 @@ function Header({darkMode, onToggleTheme}) {
   )
 }
 
-export default FitsnapTheme(Header)
+export default Header
