1fc0f29d4518ac88322e86de97ad7b54c8b63bff
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2024-09-28 16:47:06 +0530

    main fixed

--- fitsnap/src/main.jsx
+++ fitsnap/src/main.jsx
@@ -1,7 +1,7 @@
 // Importing VDOM, Provider,router&Store
+import { createRoot } from 'react-dom/client'
 import {Provider} from 'react-redux'
 import store from './store/store.js'
-import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 // Importing Global Styles
@@ -9,17 +9,11 @@ import './index.css'
 
 // Importing Components and Elements
 import App from './App.jsx'
-import Dashboard from './components/pages/Dashboard.jsx'
-
 const router = createBrowserRouter([
   {
     path:'/',
     element:<App/>
-  },
-  {
-    path: "/dashboard",
-    element:<Dashboard />
-  },
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
